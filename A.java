27289991dd3bd99c94f04b49e78e6a01ec9fb376
Mauricio Aniche <mauricioaniche@gmail.com>
2020-03-13 17:37:35 +0100

    extract and move method

--- A.java
+++ A.java
@@ -1,14 +1,22 @@
 package a;
 
+class Utils {
+        public int getC() {
+                int c = 0;
+                c = c + 1;
+                c = c + 2;
+                return c;
+        }
+}
+
+
 class A {
 
 	public void m1() {
 		int b = 0;
 		b = 10;
 
-		int c = 0;
-		c = c + 1;
-		c = c + 2;
+		int c = new Utils().getC();
 
 		System.out.println(b);
 		System.out.println(c);
